d72f9d06a446e866e1fa3a53de1547101cfb35d8
Daniel Abraham Dzul Pech <danieldayamond@gmail.com>
2022-10-27 23:27:04 -0500

    Agregar archivos de proyecto.

---
+++ WebApplication1/Data/AplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Modelo;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1.Data
+{
+    public class AplicationDbContext : DbContext
+    {
+        public AplicationDbContext(DbContextOptions<AplicationDbContext> options) :base(options) { }
+        public DbSet<Personaje> personajes { get; set; }
+        public DbSet<Genero> generos { get; set; }
+    }
+}
