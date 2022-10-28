3b3b09ee70d94cc81653df38ebc04ba4a6a6630a
DanielAbrahamDzulPech <danieldayamond@gmail.com>
2022-10-28 14:06:21 -0500

    add model genero

--- Domain/Modelo/Genero.cs
+++ Domain/Modelo/Genero.cs
@@ -7,6 +7,11 @@ using System.Threading.Tasks;
 
 namespace Domain.Modelo
 {
-   
+    public class Genero
+    {
+        [Key]
+        public int PkGenero { get; set; }
 
+        public string Nombre { get; set; }
+    }
 }
